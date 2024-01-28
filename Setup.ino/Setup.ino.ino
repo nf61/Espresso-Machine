60888d58a665cfda6132e307e2abe734aefe580d
nf61 <92657943+nf61@users.noreply.github.com>
2024-01-28 14:01:06 -0500

    Add pins and functions

--- Setup.ino/Setup.ino.ino
+++ Setup.ino/Setup.ino.ino
@@ -1,78 +1,83 @@
 #include <HX711.h>
-#include <Encoder.h>
+#include <ESP32Encoder.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <Adafruit_MAX31865.h>
 #include <Adafruit_ADS7830.h>
+#include <Bounce2.h>
+#include "EEPROM.h"
 
+// Pump Switch Pin
+const int PumpSwitch = 0; // need to decide pin
+pinMode(PumpSwitch, INPUT); 
+int SwitchState = 0;
 
+//Encoder setup
+ESP32Encoder encoder;
+const int clk = 25;
+const int dt = 33;
+
+// Instantiate eeprom to store temp variable so it remembers what the last temp was set at
+EEPROMClass  TEMPS("eeprom0");
+int SetTemp = 100; // tempurature that the water should heat up to
 
 // Use software SPI: CS, DI, DO, CLK
 Adafruit_MAX31865 thermo = Adafruit_MAX31865(15, 2, 4, 16);
 // use hardware SPI, just pass in the CS pin
 //Adafruit_MAX31865 thermo = Adafruit_MAX31865(10);
-
 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
 // The 'nominal' 0-degrees-C resistance of the sensor
 // 100.0 for PT100, 1000.0 for PT1000
-
 //TEMP
 const int RREF = 430.0;
 const int RNOMINAL = 100.0;
+
 //PRESSURE
-const int P_GPIO = 34;
+Adafruit_ADS7830 ad7830;
+
 //WEIGHT
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 19;
 const int LOADCELL_SCK_PIN = 18;
+
 //SCREEN
 const int SCREEN_WIDTH = 128;  // OLED display width, in pixels
 const int SCREEN_HEIGHT = 128; // OLED display height, in pixels
 const int OLED_RESET = -1;     // can set an oled reset pin if desired
-
 Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, 1000000, 100000);
 HX711 scale;
 
+//Timer
+long ShotTimer = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  //Serial.print('hello')
-  
   SetupScreen();
   SetupSensors();
+  SetupEncoder();
 
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- // m.display.drawPixel(10, 10, SH110X_WHITE);
- // m.display.display();
- 
-  //read from sensors
-  uint16_t rtd = thermo.readRTD();
-  float ratio = rtd;
-  ratio /= 32768;
-  //uint8_t fault = thermo.readFault();
-
-  //Serial.print("Ratio = "); Serial.println(ratio,8);
+  
+   //Serial.print("Ratio = "); Serial.println(ratio,8);
   //Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = "); Serial.println(thermo.temperature(RNOMINAL, RREF));
+  //Serial.print("Temperature = "); Serial.println(thermo.temperature(RNOMINAL, RREF));
 
   display.clearDisplay();
-  display.setTextSize(3);
+  display.setTextSize(1.5);
   display.setTextColor(SH110X_WHITE);
-  display.setCursor(10, 10);     // Start at top-left corner
+  display.setCursor(0, 10);     // Start at top-left corner
   //TEMP
-  display.print((int)(thermo.temperature(RNOMINAL, RREF) * (9./5.) + 32.)); display.println(F("F"));
+  DisplayTemp();
   //PRESSURE
-
-  
-  //display.println(analogRead(P_GPIO));
-  display.println(F("N/A BAR"));
+    display.print("PSI = "); display.println((int)((ad7830.readADCsingle(0)-41)*.6444444));
+  //SCALE
   if(scale.is_ready())
   {
       display.print(scale.get_units(),1); display.println(F("g"));
@@ -83,11 +88,6 @@ void loop() {
   }
   display.display();
   delay(1000);
-  //WEIGHT
-  
-
-
-  //write to screen
 
 
 
@@ -120,11 +120,51 @@ void SetupScreen()
   display.clearDisplay();
 }
 
+void SetupEncoder()
+{
+  ESP32Encoder::useInternalWeakPullResistors=UP;
+  encoder.attachHalfQuad(clk, dt);
+  encoder.clearCount();
+}
+
 void SetupSensors()
 {
   ////TEMP
   thermo.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  ad7830.begin();
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();  
+  
+}
+
+void DisplayTemp(){
+   //read from sensors
+  uint16_t rtd = thermo.readRTD();
+  float ratio = rtd;
+  ratio /= 32768;
+  //uint8_t fault = thermo.readFault();
+  display.print("Temp = ");
+  display.print((float)((thermo.temperature(RNOMINAL, RREF) * (9./5.)) + 32.));
+  display.println(F(" F"));
+}
+
+void DisplayTime()
+{
+  display.print("Shot Time = ");
+  display.println((long)((SHOTTIMER));
+
+}
+
+void StartTimer()
+{
+  long InitialVal = millis();
+  if(SwitchState == 1)
+  {
+   ShotTimer = millis() - InitialVal; 
+    
+  }
+  else{
+    ShotTimer = 0;
+  }
 }
